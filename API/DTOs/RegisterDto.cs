c3297e08403d35f47cae3c58ab96dff89652f099
Steve D <steve.diersen@intelerad.com>
2024-03-18 08:15:16 -0500

    End of Section 13

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,7 +8,7 @@ namespace API.DTOs
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must contain a number")]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex.")]
         public string Password { get; set; }
         [Required]
         public string DisplayName { get; set; }
